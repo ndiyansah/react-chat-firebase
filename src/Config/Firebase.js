d5c9408904e7d9e0e2e32a1149f8fbc60cac5826
ndiyansah <handi@Betanias-MacBook-Air.local>
2018-02-03 17:42:36 +0700

    config

---
+++ src/Config/Firebase.js
@@ -0,0 +1,10 @@
+  var config = {
+    apiKey: "AIzaSyDsHJsPK_Sr7b63bEIH7ngmgYm6JmwXp7A",
+    authDomain: "react-chat-firebase-dd962.firebaseapp.com",
+    databaseURL: "https://react-chat-firebase-dd962.firebaseio.com",
+    projectId: "react-chat-firebase-dd962",
+    storageBucket: "react-chat-firebase-dd962.appspot.com",
+    messagingSenderId: "169263849622"
+  };
+
+  export default config;
